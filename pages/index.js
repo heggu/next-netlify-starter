f4f9ace145f4ef52de3eeb70efae9c26fe2c92cd
heggu <115707064+heggu@users.noreply.github.com>
2023-02-23 13:01:24 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -10,10 +10,10 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main>
-        <Header title="Welcome to my site! -Jihoon" />
+     <main>
+        <Header title="Welcome to my app!" />
         <p className="description">
-         <img src="https://cdn.discordapp.com/attachments/1030350459950600222/1077913389122203689/download.png" class="Footer_logo__AdUui"> 
+          Get started by editing <img src="https://cdn.discordapp.com/attachments/1030350459950600222/1077913389122203689/download.png" class="Footer_logo__AdUui">
         </p>
       </main>
 
