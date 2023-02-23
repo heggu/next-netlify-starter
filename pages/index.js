b3432ed273bd2ab37a558748deb7e505802bfb18
heggu <115707064+heggu@users.noreply.github.com>
2023-02-23 13:09:25 +0900

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
      <main>
         <Header title="Welcome to my app!" />
         <p className="description">
-          Get started by editing <img src="https://cdn.discordapp.com/attachments/1030350459950600222/1077913389122203689/download.png" class="Footer_logo__AdUui">
+         <img src="https://cdn.discordapp.com/attachments/1030350459950600222/1077913389122203689/download.png" class="Footer_logo__AdUui">
         </p>
       </main>
 
